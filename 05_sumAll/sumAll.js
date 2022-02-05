23cff8106eca734d501086fa4b7a57b2240ac79d
Angelo <iamanauthor9@gmail.com>
2022-02-05 18:52:55 +0800

    COMPLETED!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,18 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    if (num2 > num1) {
-        for (let i = num1; num1 <= num2; i++) {
-            sum += num1
+    if (num1 < 0 || num2 < 0 || !(Number.isInteger(num1)) || !(Number.isInteger(num2))) {
+        return 'ERROR';
+    } else if (num2 > num1) {
+        let currentNum = num1;
+        while (currentNum <= num2) {
+            sum += currentNum;
+            currentNum++;
+        }
+    } else if (num1 > num2) {
+        let currentNum = num2;
+        while (currentNum <= num1) {
+            sum += currentNum;
+            currentNum++;
         }
     }
     return sum;
