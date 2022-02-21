241c3e3dc812b0218d79528ca292fa4283fa98db
Angelo Curio <iamanauthor9@gmail.com>
2022-02-21 23:59:42 +0800

    COMPLETED!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (...number) {
+  let sum = 0;
+  for (num of number) {
+    sum += num;
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (...number) {
+  return number.reduce((diff, num) => diff - num);
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.length === 0 ? 0 : numbers.reduce((sum, num) => sum + num);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function (...numbers) {
+  return numbers.reduce((power, powerator) => power ** powerator);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) return 1;
+  else {
+  }
+  let numbers = [];
+  for (i = 1; i <= number; i++) {
+    numbers.push(i);
+  }
+  return numbers.reduce((product, num) => product * num);
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
