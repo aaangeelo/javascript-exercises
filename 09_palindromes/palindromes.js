63a5440329f6242acacb13b6b5a62daa471da1ed
Angelo Curio <iamanauthor9@gmail.com>
2022-02-22 09:54:18 +0800

    COMPLETED!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let toArr = str
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s+/g, "")
+    .split("");
+  if (toArr.join("") === toArr.reverse().join("")) return true;
+  else return false;
 };
 
 // Do not edit below this line
