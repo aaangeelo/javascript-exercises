abbdac21359497f3ae91321804a7d892ea82e18d
Angelo Curio <iamanauthor9@gmail.com>
2022-02-22 21:00:28 +0800

    COMPLETED!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const findCurrentAge = (person) => {
+    if (!person.yearOfDeath) {
+      const date = new Date();
+      return date.getFullYear() - person.yearOfBirth;
+    } else return person.yearOfDeath - person.yearOfBirth;
+  };
+  const oldestAge = people
+    .map((person) => findCurrentAge(person))
+    .sort()
+    .pop();
+  const oldestPerson = people.filter((person) => {
+    if (findCurrentAge(person) === oldestAge) return true;
+  });
+  return oldestPerson[0];
 };
 
 // Do not edit below this line
