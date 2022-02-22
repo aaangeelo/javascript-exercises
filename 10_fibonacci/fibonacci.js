fe6de60004a95e9bd9108e28abe8bde354755d40
Angelo Curio <iamanauthor9@gmail.com>
2022-02-22 13:58:15 +0800

    COMPLETED!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  let numbers = [1, 1];
+  for (i = 2; i < number; i++) {
+    let lastNum = numbers[numbers.length - 1];
+    let secondLast = numbers[numbers.length - 2];
+    numbers.push(lastNum + secondLast);
+  }
+  return numbers.pop();
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+// console.log(fibonacci(15));
